58ce366d1e51d62e1d2e6c797ba676433314a653
Yash Garg <yash431garg@gmail.com>
2024-02-04 15:02:42 +0530

    code optimized

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,90 +1,57 @@
-// import mongoose from 'mongoose';
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 import { ethers } from 'ethers';
 import { EAS, SchemaEncoder } from '@ethereum-attestation-service/eas-sdk';
 
-// import connectDB from '../../utils/connectDb';
-
-// const Opinion = mongoose.model(
-//   'Opinion',
-//   new mongoose.Schema({
-//     newlabel: { type: String },
-//     newaccountAddress: { type: Number },
-//   }),
-// );
-
 const NEXT_PUBLIC_URL = process.env.APP_URL;
 const WALLET_KEY = process.env.WALLET_PRIVATE_KEY || '';
+const EAS_CONTRACT_ADDRESS = '0xC2679fBD37d54388Ce493F1DB75320D236e1815e';
+
+interface Emotion {
+  label: string;
+}
+
+const emotions: Emotion[] = [
+  { label: 'Extreme Fear' },
+  { label: 'Fear' },
+  { label: 'Greed' },
+  { label: 'Extreme Greed' },
+];
+
+function getLabelByIndex(index: number): string {
+  return emotions[index]?.label || 'Invalid index';
+}
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
   let accountAddress: number | '';
   let index: number = 1;
   let label: string = '';
-
-  // await connectDB();
-  const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   if (isValid) {
     accountAddress = message.interactor.fid;
   }
 
-  const EAS_CONTRACT_ADDRESS = '0xC2679fBD37d54388Ce493F1DB75320D236e1815e';
   const provider = new ethers.JsonRpcProvider(process.env.QUICKNODE_ENDPOINT);
   const signer = new ethers.Wallet(WALLET_KEY, provider);
   const eas = new EAS(EAS_CONTRACT_ADDRESS);
   eas.connect(signer);
-  const schemaUID = '0x32bb7727b5d0ba7f8851cc67439dfbc90ec1549a749ef329e42c0c298868c627';
 
-  // Se
-
-  interface Emotion {
-    label: string;
-  }
-
-  const emotions: Emotion[] = [
-    { label: 'Extreme Fear' },
-    { label: 'Fear' },
-    { label: 'Greed' },
-    { label: 'Extreme Greed' },
-  ];
-
-  function getLabelByIndex(index: number): string {
-    // Make sure the index is within the valid range
-    if (index >= 0 && index < emotions.length) {
-      return emotions[index].label;
-    } else {
-      return 'Invalid index';
-    }
-  }
   if (body?.untrustedData) {
     index = body.untrustedData.buttonIndex - 1;
     label = getLabelByIndex(index);
-    // try {
-    //   const newOpinion = new Opinion({
-    //     newaccountAddress: accountAddress,
-    //     newlabel: label,
-    //   });
-
-    //   await newOpinion.save();
-    //   console.log('Opinion saved successfully');
-    // } catch (error) {
-    //   console.error('Error saving opinion:', error);
-    // }
   }
 
-  // Initialize SchemaEncoder with the schema string
+  const schemaDefinition = 'uint256 pollId, string about, string feeling, string dateOfPoll';
 
-  const tx = await eas.attest({
-    schema: schemaUID,
+  await eas.attest({
+    schema: '0x32bb7727b5d0ba7f8851cc67439dfbc90ec1549a749ef329e42c0c298868c627',
     data: {
       recipient: '0x229922ef85a6660054F0e6cF67b3580635056c15',
-
       revocable: true,
-      data: new SchemaEncoder(
-        'uint256 pollId, string about, string feeling, string dateOfPoll',
-      ).encodeData([
+      data: new SchemaEncoder(schemaDefinition).encodeData([
         { name: 'pollId', value: message?.interactor.fid || 0, type: 'uint256' },
         { name: 'about', value: 'How do you feel about Crypto 2024', type: 'string' },
         { name: 'feeling', value: label || 'MOOD', type: 'string' },
@@ -92,10 +59,6 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       ]),
     },
   });
-
-  // const newAttestationUID = await tx.wait();
-  // console.log('New attestation UID:', newAttestationUID);
-  console.log('error');
 
   return new NextResponse(
     getFrameHtmlResponse({
@@ -105,7 +68,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
       ],
       image: `${NEXT_PUBLIC_URL}/indicator${index + 1}.png`,
-      post_url: `${NEXT_PUBLIC_URL}/api/attest`,
+      post_url: `${NEXT_PUBLIC_URL}`,
     }),
   );
 }
